2100e709730c7db51e525d54659551597c798543
Bayu adi waskito <bayuadiwaskitob@gmail.com>
2024-09-09 11:37:17 +0700

    game1

---
+++ game/src/Pages/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className='bg-red-700'>
+            <p className='text-9xl'>oooooooooooooo</p>
+        </div>
+    )
+}
+
+export default Login
