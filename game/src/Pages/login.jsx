a4ce64a81e86c9dd84cdb5bb5571fd3eecb4d458
Bayu adi waskito <bayuadiwaskitob@gmail.com>
2024-09-09 11:45:36 +0700

    perubahan 1
    merubah text

--- game/src/Pages/login.jsx
+++ game/src/Pages/login.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
     return (
         <div className='bg-red-700'>
-            <p className='text-9xl'>oooooooooooooo</p>
+            <p className='text-9xl'>mencoba react</p>
         </div>
     )
 }
