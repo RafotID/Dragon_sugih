e7fd701f039d0ce5b09a6b87730ea5c4bdd49ffa
Bayu adi waskito <bayuadiwaskitob@gmail.com>
2024-09-11 20:33:06 +0700

    feat : update "menambahkan halaman privasi dan menambahkan responsive"

--- FrontEnd/src/Pages/Privasi.jsx
+++ FrontEnd/src/Pages/Privasi.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React from 'react';
 import { assets } from '../assets/indeks';
+
 const Privasi = () => {
     return (
         <>
@@ -8,43 +9,52 @@ const Privasi = () => {
                 style={{ backgroundImage: `url(${assets.gambar.background1})` }}
             >
                 {/* Gambar Caifu di kanan atas */}
-                <div className='absolute top-0 right-0 m-4'>
-                    <img src={assets.gambar.dragonCaifu} alt="Dragon Caifu"
-                        className='w-[150px] md:w-[200px] z-5' />
+                <div className='absolute top-0 right-0 m-2 sm:m-4'>
+                    <img 
+                        src={assets.gambar.dragonCaifu} 
+                        alt="Dragon Caifu"
+                        className='w-[100px] sm:w-[150px] md:w-[200px] z-10' 
+                    />
                 </div>
 
-                <div className='bg-custom-gray bg-opacity-40 w-1250 h-730 z-10 flex-col'>
-                    <div className='bg-custom-green w-315 h-179 text-3xl flex'>
-                        <p>Pengingat privasi</p>
-                    </div>
+                <div className='relative flex justify-center items-center min-h-screen p-4 sm:p-6 md:p-10'>
+                    {/* Kontainer yang lebih responsif */}
+                    <div className='bg-custom-gray bg-opacity-70 w-full max-w-[95%] sm:max-w-[800px] min-h-[450px] sm:min-h-[500px] md:min-h-[530px] z-10 flex-col border-2 border-black'>
+                        <div className='bg-custom-green w-full h-[80px] sm:h-[100px] text-xl sm:text-2xl md:text-3xl flex items-center'>
+                            <p className='ml-4 sm:ml-5 text-yellow-400 font-inter font-bold'>Pengingat privasi</p>
+                        </div>
 
-                    <p>Harap diperhatikan bahwa kami mengumpulkan dan menggunakan data
-                        berikut:
+                        <p className="p-5 sm:p-7 text-lg sm:text-xl md:text-2xl font-itim">
+                            Harap diperhatikan bahwa kami mengumpulkan dan menggunakan data berikut:
+                        </p>
 
-                        Nama akun, Alamat email, Sandi
+                        <ul className="list-disc list-inside px-6 sm:px-8 md:px-10 text-lg sm:text-xl md:text-2xl font-itim">
+                            <li>Nama akun, Alamat email, Sandi</li>
+                        </ul>
 
-                        Data ini digunakan untuk:
+                        <p className='pt-5 sm:pt-7 font-itim pl-8 sm:pl-11 text-lg sm:text-xl md:text-2xl'>
+                            Data ini digunakan untuk:
+                        </p>
 
-                        Identified akun, Register, Login
-                    </p>
+                        <ul className="list-disc list-inside px-6 sm:px-8 md:px-10 pt-5 sm:pt-8 text-lg sm:text-xl md:text-2xl font-itim">
+                            <li>Identified akun, Register, Login</li>
+                        </ul>
 
-                    <div>
-                        <div>
-
+                        <div className='flex pt-8 sm:pt-10 justify-center'>
+                            <div className='flex justify-between w-[80%] sm:w-[65%] md:w-[70%] text-lg sm:text-xl md:text-2xl text-white font-inter font-bold'>
+                                <button className='bg-custom-buton-tolak w-[120px] sm:w-[140px] md:w-[150px] h-[40px] sm:h-[45px] md:h-[50px]'>
+                                    Tolak
+                                </button>
+                                <button className='bg-custom-buton-trima w-[120px] sm:w-[140px] md:w-[150px] h-[40px] sm:h-[45px] md:h-[50px]'>
+                                    Terima
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-
-
-
-
             </div>
-
-
         </>
-
-    )
+    );
 }
 
-export default Privasi
+export default Privasi;
--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -10,10 +10,14 @@ export default {
         'custom-gray': '#D9D9D9',
         'custom-black': '#2F4F4F',
         'custom-green': '#537577',
+        'custom-buton-tolak': '#B06A4E',
+        'custom-buton-trima': '#5920B5',
       },
       fontFamily: {
         'inria-serif': ['"Inria Serif"', 'serif'],
-        'inika': ['"Inika"']
+        'inika': ['"Inika"'],
+        'itim' : ['"Itim"'],
+        'inter' : ['"Inter"']
       },
     },
   },
